7ebdf937c88db5f334e9d6d2a558dc8bec46fa95
oguzhanural <oguzhnural@gmail.com>
2023-05-11 23:56:08 +0300

    some design part updated

---
+++ app/components/Button.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+    label: string;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    disable?: boolean;
+    outline?: boolean;
+    small?: boolean;
+    icon?: IconType;
+
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    onClick,
+    disable,
+    outline,
+    small,
+    icon
+}) => {
+  return (
+   <button className={`
+   relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition
+   w-full
+   ${outline ? 'bg-white' : 'bg-rose-500'}
+   ${outline ? 'border-black' : 'border-rose-500'}
+   ${outline ? 'text-black' : 'text-white'}
+   ${small ? 'py-1' : 'py-3'}
+   ${small ? 'text-sm' : 'text-xl'}
+   ${small ? 'font-light' : 'font-semibold'}
+   `}>
+    {label}
+   </button>
+  )
+}
+
+export default Button
