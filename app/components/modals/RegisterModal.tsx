1ff8d2fdc9f8ac3e0e7e8ea0ed101a9834fd1165
oguzhanural <oguzhnural@gmail.com>
2023-05-19 00:04:44 +0300

    added google, github button

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -13,6 +13,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 
 const RegisterModal = () => {
@@ -85,8 +86,33 @@ const RegisterModal = () => {
 
     // Footer Content for login like Google, Facebook, Email, Apple etc.
     const footerContent = (
-        <div className="">
-            
+        <div className="flex flex-col gap-4 mt-3">
+           <hr />
+           <Button 
+            outline
+            label="Continue with Google"
+            icon={FcGoogle}
+            onClick={ () => {}}
+           />
+           <Button 
+            outline
+            label="Continue with Github"
+            icon={AiFillGithub}
+            onClick={ () => {}}
+           /> 
+            <div className="text-neutral-500 text-center mt-4 font-light">
+                <div className=" justify-center flex flex-row items-center gap-2">
+                    <div className="">
+                        Already have an account?
+                    </div>
+                    <div 
+                    onClick={registerModal.onClose}
+                    className="text-neutral-800 cursor-pointer hover:underline">
+                        Log in
+                    </div>
+                </div>
+                
+            </div>
         </div>
     );
 
@@ -100,6 +126,7 @@ const RegisterModal = () => {
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
         body={bodyContent}
+        footer={footerContent}
     />
   )
 }
