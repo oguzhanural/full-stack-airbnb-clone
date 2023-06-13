d15f36ec9ba9f8ff17bd66d04b78679d2d0d73ef
oguzhanural <oguzhnural@gmail.com>
2023-06-13 20:00:54 +0300

    updated

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -36,7 +36,7 @@ const UserMenu: React.FC<UserMenuProps>  = ({
             onClick={toggleOpen}>
                 <AiOutlineMenu />
                 <div className="hidden md:block">
-                    <Avatar />
+                    <Avatar src={currentUser?.image} />
                 </div>
             </div>
         </div>
