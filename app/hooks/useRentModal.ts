05c5019bd320ed76ebea87232ffa80209cfc0a03
oguzhanural <oguzhnural@gmail.com>
2023-06-14 22:36:19 +0300

    created userRentModal component

---
+++ app/hooks/useRentModal.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+// import { devtools, persist } from 'zustand/middleware';
+
+interface RentModalStore {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+
+}
+
+const useRentModal = create<RentModalStore>((set) =>({
+    isOpen: false,
+    onOpen: () => set({isOpen: true}),
+    onClose: () => set({isOpen: false}),
+
+}));
+
+export default useRentModal;
