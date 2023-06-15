861ace6ec30ccf2a4b3f13ac85aa705bb43078e5
oguzhanural <oguzhnural@gmail.com>
2023-06-15 23:09:38 +0300

    Rent modal updated

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,17 +2,68 @@
 
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
+import { useMemo, useState } from "react";
+import Heading from "../Heading";
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5
+}
 
 const RentModal = () => {
     const rentModal = useRentModal();
+    const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const onBack = () => {
+        setStep((value:number) => value - 1);
+    }
+
+    const onNext = () => {
+        setStep((value:number) => value + 1);
+    }
+
+    const actionLabel = useMemo( () => {
+        if (step === STEPS.PRICE) {
+            return 'Create';
+        }
+        return 'Next';
+    }, [step]);
+
+    const secondaryActionLabel = useMemo( () => {
+        if (step === STEPS.CATEGORY) { // if we are first step, there is no back button.
+            return undefined;
+        }
+        return 'Back';
+    }, [step]);
+    
+    let bodyContent = (
+        <div className="flex flex-col gap-8">
+            <Heading
+            title="Which of these best describes of your place?"
+            subtitle="Pick a category"
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+                {} // map fonksiyonu ile kategorileri buraya ekleyeceğiz
+            </div>
+        </div>
+    )
+    
+
   return (
 
     <Modal 
     onClose={rentModal.onClose}
     onSubmit={rentModal.onClose}
     isOpen = {rentModal.isOpen}
-    actionLabel="Submit"
+    actionLabel={actionLabel}
+    secondaryActionLabel={secondaryActionLabel}
+    secondaryAction={step === STEPS.CATEGORY ? undefined : onBack } 
     title="Airbnb your home!"
+    body={bodyContent}
     />
 
   );
