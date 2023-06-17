edcb0332effbc90cd9f44f51eb4615380bd38f1e
oguzhanural <oguzhnural@gmail.com>
2023-06-17 14:13:05 +0300

    rent modal category design was done

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -4,6 +4,8 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
 
 enum STEPS {
     CATEGORY = 0,
@@ -47,7 +49,18 @@ const RentModal = () => {
             subtitle="Pick a category"
             />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
-                {} // map fonksiyonu ile kategorileri buraya ekleyeceğiz
+                {/* // map fonksiyonu ile kategorileri buraya ekleyeceğiz */}
+                {categories.map((item) => (
+                    <div key={item.label} className="col-span-1">
+                       <CategoryInput
+                        onClick = {() => {}}
+                        selected = {false}
+                        label = {item.label}
+                        icon = {item.icon}
+                       />
+                    </div>
+                ))} 
+                
             </div>
         </div>
     )
