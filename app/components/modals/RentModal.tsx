1f79d5feb50f168c1557be1a5c9d035fc6f51418
oguzhanural <oguzhnural@gmail.com>
2023-06-22 11:22:55 +0300

    counter component updated

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -54,6 +54,8 @@ const RentModal = () => {
     const category = watch('category');
     const location = watch('location');
     const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
     //lokasyon değiştirildikçe harita da ona göre değişecek
     // o yüzden useMemo dependincies i lokasyona bağlı.
@@ -148,6 +150,22 @@ const RentModal = () => {
                   value={guestCount}
                   onChange={(value) => setCustomValue('guestCount', value)}
                  /> 
+                 <hr />
+                 <Counter
+                  title="Rooms"
+                  subtitle="How many rooms do you have?"
+                  value={roomCount}
+                  onChange={(value) => setCustomValue('roomCount', value)}
+                 /> 
+                 <hr />
+                 <Counter
+                  title="Bathrooms"
+                  subtitle="How many bathrooms do you have?"
+                  value={bathroomCount}
+                  onChange={(value) => setCustomValue('bathroomCount', value)}
+                 /> 
+                 <hr />
+
 
             </div>
         )
