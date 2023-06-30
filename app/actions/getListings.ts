94e7d96d7384aa3518f84749e37f83849fa039e6
oguzhanural <oguzhnural@gmail.com>
2023-06-30 15:44:08 +0300

    get listings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -3,9 +3,12 @@ import prisma from "@/app/libs/prismadb";
 export default async function getListing() {
     try {
         // fetch listings
-        const listings = await prisma.listing.findMany()
-
-
+        const listings = await prisma.listing.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        return listings; 
     } catch (error: any) {
         throw new Error(error);
     }
