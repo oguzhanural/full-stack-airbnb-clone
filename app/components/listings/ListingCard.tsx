7800c6ded053b33a6dfe47c67c5abc3fe2002ceb
oguzhanural <oguzhnural@gmail.com>
2023-07-04 01:16:12 +0300

    listing card changed

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -6,6 +6,8 @@ import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { format } from 'date-fns';
+import Image from "next/image";
+import HeartButton from "../HeartButton";
 
 interface ListingCardProps {
     data: Listing;
@@ -63,7 +65,39 @@ const ListingCard:React.FC<ListingCardProps> = ({
   return (
     <div
     onClick={()=>router.push(`/listings/${data.id}`)}
-     className="col-span-1 cursor-pointer grup">
+     className="col-span-1 cursor-pointer group">
+        <div className="flex flex-col gap-2 w-full">
+            <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+                <Image 
+                 alt="Listing"
+                 src={data.imageSrc}
+                 fill
+                 className="object-cover h-full w-full group-hover:scale-110 transition"
+                />
+                <div className="absolute top-3 right-3">
+                    <HeartButton
+                        listingId={data.id}
+                        currentUser = {currentUser}
+                    />
+                </div>
+            </div>
+            <div className="font-semibold text-lg">
+                {location?.region}, {location?.label}
+            </div>
+            <div className="font-light text-neutral-500">
+                {reservationDate || data.category}
+            </div>
+            <div className="flex flex-row items-center gap-1">
+                <div className="font-semibold">
+                    $ {price}
+                </div>
+                {!reservation && (
+                    <div className="font-light">night</div>
+                )}
+            </div>
+            {}
+
+        </div>
 
     </div>
   )
