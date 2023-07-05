ab5a873b065117a82552e5a9d0d7c7b1439e33bc
oguzhanural <oguzhnural@gmail.com>
2023-07-05 19:05:57 +0300

    Done useFavorite hook

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+import { SafeUser } from "../types";
+ 
+import useLoginModal from "./useLoginModal";
+
+interface IUseFavorite {
+    listingId: string;
+    currentUser?: SafeUser | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+    const router = useRouter();
+    const loginModal = useLoginModal();
+
+    const hasFavorited = useMemo(() => {
+        const list = currentUser?.favoriteIds || []; // daha önceden favorilere eklemediği için boş array döner.
+        return list.includes(listingId);    // listingId değerini içermediği için - çünkü boş - false dönüyor.
+    }, [currentUser, listingId]);
+
+
+    const toggleFavorite = useCallback(async (e:React.MouseEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+
+        if(!currentUser) {
+            // login modal will open if tries to add to favorites without logging in.
+            return loginModal.onOpen();
+        }
+
+        try {
+            let request;
+
+            if (hasFavorited) {
+                request = () => axios.delete(`/api/favorites/${listingId}`);
+            } else {
+                request = () => axios.post(`/api/favorites/${listingId}`);
+            }
+            await request();
+            router.refresh();
+            toast.success('Success');
+
+        } catch (error) {
+            toast.error('Something went wrong');
+        }
+
+    }, [currentUser, hasFavorited, listingId, loginModal, router]);
+    // console.log(hasFavorited);
+
+    return {
+        hasFavorited,
+        toggleFavorite
+    }
+
+}
+
+export default useFavorite;
