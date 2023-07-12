c40deb2867414e4797ba86be5488163be0e3e2a6
oguzhanural <oguzhnural@gmail.com>
2023-07-12 16:21:20 +0300

    data type changed to SafeListing

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -2,7 +2,7 @@
 
 import { useCallback, useMemo } from "react";
 import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { format } from 'date-fns';
@@ -11,7 +11,7 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-    data: Listing;
+    data: SafeListing;
     reservation?: Reservation;
     onAction?: (id: string) => void;
     disabled?: boolean;
