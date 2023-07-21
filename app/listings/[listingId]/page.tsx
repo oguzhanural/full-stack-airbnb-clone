fa7be1c60efb4c193a11946ef637ed5dd23afb74
oguzhanural <oguzhnural@gmail.com>
2023-07-21 23:35:28 +0300

    added reservations and listing client props

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -4,16 +4,23 @@ import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingId?: string;
 }
 
+
 // we can still access the parameters in server components. Which in our cases is the URL here.
+
 const ListingPage = async ({ params }: {params:IParams}) => {
 
   // we can only use actions like this which directly comminicate with the database.
   const listing = await getListingById(params);
+
+  // listingId ye göre rezervasyonları bulur ve döndürür.
+  const reservations = await getReservations(params); 
+
   const currentUser = await getCurrentUser();
 
   if (!listing) {
@@ -29,6 +36,7 @@ const ListingPage = async ({ params }: {params:IParams}) => {
       <ListingClient 
         listing={listing}
         currentUser={currentUser}
+        reservations={reservations}
       />
 
     </ClientOnly>
