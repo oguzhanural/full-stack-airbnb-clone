c00adb1518f3886456df8f16536ff3654d8902ea
oguzhanural <oguzhnural@gmail.com>
2023-08-10 22:51:51 +0300

    get listing updated for users filter

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,17 +2,76 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
     userId?:string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
     try {
-        const { userId } = params;
+        const { 
+            userId,
+            guestCount,
+            roomCount,
+            bathroomCount,
+            startDate,
+            endDate,
+            locationValue,
+            category
+         } = params;
 
         let query: any = {};
 
         if (userId) {
             query.userId= userId;
         }
+        if (category) {
+            query.category= category;
+        }
+        if (roomCount) {
+            query.roomCount = {
+                gte: +roomCount // greater than or equal (gte)
+            }
+        }
+        if (guestCount) {
+            query.guestCount = {
+                gte: +guestCount // greater than or equal (gte)
+            }
+        }
+        if (bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount // greater than or equal (gte)
+            }
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue;
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: { gte: startDate},
+                                startDate: {lte: startDate}
+                            },
+                            {
+                                startDate: { lte: endDate},
+                                endDate: { gte: endDate} 
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+
+        
         // fetch listings
         const listings = await prisma.listing.findMany({
             where: query,
@@ -20,7 +79,7 @@ export default async function getListings(params: IListingsParams) {
                 createdAt: 'desc'
             }
         });
-
+        
         const safeListings = listings.map((listing) => ({
             ...listing,
             createdAt: listing.createdAt.toISOString(),
